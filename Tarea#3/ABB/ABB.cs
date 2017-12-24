1123d23bf1903a22f843dce8403c3e0fa881b219
kepler6309 <ryusfv@gmail.com>
2017-12-23 22:30:12 -0600

    tarea #3 del laboratorio de EDD

---
+++ Tarea#3/ABB/ABB.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABB
+{
+    public class ABB
+    {
+        private class node
+        {
+            public node(String data_)
+            {
+                data = data_;
+                left = null;
+                right = null;
+            }
+            private node left;
+            public node Left
+            {
+                get { return left;}
+                set { left = value; }
+            }
+            private node right;
+            public node Right
+            {
+                get { return right; }
+                set { right = value; }
+            }
+            private String data;
+            public String Data
+            {
+                get { return data; }
+                set { data = value; }
+            }
+        }
+
+        private node root;
+
+        public void add(String data)
+        {
+            root = add(root, data);
+        }
+
+        node add(node aux, String data)
+        {
+            if (aux == null)
+            {
+                aux = new node(data);
+                return aux;
+            }
+            else if(String.Compare(data,aux.Data)<0)
+            {
+                aux.Left = add(aux.Left, data);                 
+                return aux;
+            }
+            else if (String.Compare(data, aux.Data) > 0)
+            {
+                aux.Right = add(aux.Right, data);
+                return aux;
+            }
+            else
+            {
+                return aux;
+            }
+        }
+
+        public void enorden()
+        {
+            Console.Write("[ ");
+            enorden(root);
+            Console.Write("]");
+            Console.WriteLine();
+        }
+        public void posorden()
+        {
+            Console.Write("[ ");
+            posorden(root);
+            Console.Write("]");
+            Console.WriteLine();
+        }
+        public void preorden()
+        {
+            Console.Write("[ ");
+            preorden(root);
+            Console.Write("]");
+            Console.WriteLine();
+        }
+
+        void enorden(node aux)
+        {            
+            if (aux != null)
+            {
+                enorden(aux.Left);
+                Console.Write(aux.Data+" ");
+                enorden(aux.Right);
+            }
+        }
+        void posorden(node aux)
+        {
+            if (aux != null)
+            {
+                posorden(aux.Left);
+                posorden(aux.Right);
+                Console.Write(aux.Data + " ");                
+            }            
+        }
+        void preorden(node aux)
+        {
+            if (aux != null)
+            {
+                Console.Write(aux.Data + " ");
+                preorden(aux.Left);                
+                preorden(aux.Right);
+            }
+        }
+    }
+}
---
+++ Tarea#3/ABB/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ABB arbol = new ABB();
+            String i = " ";
+            while (i != "0")
+            {
+                Console.WriteLine("1: Insertar elemento.");
+                Console.WriteLine("2: Enorden.");
+                Console.WriteLine("3: Posorden.");
+                Console.WriteLine("4: Preorden.");
+                Console.WriteLine("5: Salir.");
+                Console.Write(">> ");
+                i = Console.ReadLine();
+                switch(i)
+                {
+                    case "1":
+                        Console.WriteLine("Ingrece una letra");
+                        Console.Write(">> ");
+                        String j = Console.ReadLine();
+                        arbol.add(j);
+                        break;
+                    case "2":
+                        arbol.enorden();
+                        break;
+                    case "3":
+                        arbol.posorden();
+                        break;
+                    case "4":
+                        arbol.preorden();
+                        break;
+                    default:
+                        i = "0";
+                        break;
+                }
+            }
+        }
+    }
+}
